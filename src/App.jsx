a79979c61ac2f86502bb06ed489fd805588ce8cc
MooreKyle <moorekl@protonmail.com>
2024-01-31 21:22:21 -0500

    Step 2: Add a Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import './App.css';
+import { useState } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0)
+  const [multiplier, setMultiplier] = useState(1)
+  const updateCount = () => setCount(count + multiplier);
 
   return (
     <div className="App">
       <div className='header'>
         <h1>Samosa Selector</h1>
-        <h2>Count:</h2>
-        <img className="Samosa" src="https://helpful-griffin-0a0ba5.netlify.app/samosa.png" alt="" />
+        <h2>Count: {count}</h2>
+        <img className="samosa" src="https://helpful-griffin-0a0ba5.netlify.app/samosa.png" alt="" onClick={updateCount} />
       </div>
     
     </div>
 
-  );
+  )
 }
 
-export default App;
+export default App
