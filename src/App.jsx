336e8a4d442718e62d5ad20fa11a263b9d6f393d
MooreKyle <moorekl@protonmail.com>
2024-02-01 03:18:10 -0500

    Step 3: Add Upgrades & Step 4: Handle Upgrades

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,24 @@ const App = () => {
   const [multiplier, setMultiplier] = useState(1)
   const updateCount = () => setCount(count + multiplier);
 
+  const buyDoubleStuffed = () => {
+    if (count >= 10) {
+      setMultiplier(multiplier * 2);
+    }
+  }
+
+  const buyPartyPack = () => {
+    if (count >= 100) {
+      setMultiplier(multiplier * 5);
+    }
+  }
+
+  const buyFullFeast = () => {
+    if (count >= 1000) {
+      setMultiplier(multiplier * 10);
+    }
+  }
+
   return (
     <div className="App">
       <div className='header'>
@@ -13,9 +31,24 @@ const App = () => {
         <h2>Count: {count}</h2>
         <img className="samosa" src="https://helpful-griffin-0a0ba5.netlify.app/samosa.png" alt="" onClick={updateCount} />
       </div>
-    
+      <div className='container'>
+        <div className='upgrade'>
+          <h3>Double Stuffed 👯‍♀️</h3>
+          <p>2x per click</p>
+          <button onClick={buyDoubleStuffed}>10 Samosas</button>
+        </div>
+        <div className='upgrade'>
+          <h3>Party Pack 🎉</h3>
+          <p>5x per click</p>
+          <button onClick={buyPartyPack}>100 Samosas</button>
+        </div>
+        <div className='upgrade'>
+          <h3>Full Feast 👩🏽‍🍳</h3>
+          <p>10x per click</p>
+          <button onClick={buyFullFeast}>1000 Samosas</button>
+        </div>
+      </div>
     </div>
-
   )
 }
 
