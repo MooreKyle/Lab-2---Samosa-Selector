8a4114ba4310fe96ba4a0c4b9604032e48404eec
MooreKyle <moorekl@protonmail.com>
2024-01-31 19:23:42 -0500

    Step 1: Update the Starter Code for Root File App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,15 @@ const App = () => {
 
   return (
     <div className="App">
-
+      <div className='header'>
+        <h1>Samosa Selector</h1>
+        <h2>Count:</h2>
+        <img className="Samosa" src="https://helpful-griffin-0a0ba5.netlify.app/samosa.png" alt="" />
+      </div>
+    
     </div>
-  )
+
+  );
 }
 
-export default App
+export default App;
